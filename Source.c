057b4333273dd4d8340be7fc1b62bd6ad62edde6
DraghiAndreas <andydraghi11@gmail.com>
2025-11-01 13:05:45 +0200

    Improved

--- Source.c
+++ Source.c
@@ -2,18 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 //EXTRA
+typedef enum {UPPER,LOWER,CAP,SWAP}STRING_MODE;
+
 void efSwap(char* v1, char* v2);
 
 // INTS
-//--------// EasyLen, EasyCmp, EasyNCmp, EasyCount, EasyStrCount | TODO : EasyIsLet
+//--------// EasyLen, EasyCmp, EasyNCmp, EasyCount, EasyStrCount,EasyIsLet | TODO : 
 
 
 int EasyLen(const char* string) {
     char* p = string;
-    while(*string) {
+    while (*string) {
         string++;
     }
-    return string-p;
+    return string - p;
 }
 
 int EasyCmp(const char* string1, const char* string2) {
@@ -33,7 +35,7 @@ int EasyNCmp(const char* string1, const char* string2, int n) {
     return(*string1 == *string2);
 }
 
-int EasyCount(const char* string,const char c) {
+int EasyCount(const char* string, const char c) {
     int count = 0;
     while (*string++) {
         if (*string == c) {
@@ -43,8 +45,8 @@ int EasyCount(const char* string,const char c) {
     return count;
 }
 
-int EasyStrCount(const char* string, const char *c) {
-    int count = 0, len = EasyLen(c) ;
+int EasyStrCount(const char* string, const char* c) {
+    int count = 0, len = EasyLen(c);
 
     while (*string) {
         const char* s = string;
@@ -67,23 +69,25 @@ int EasyStrCount(const char* string, const char *c) {
 }
 
 int EasyIsLet(const char* c) {
-    return ((*c >= 'a' && *c <= 'z') || (*c >= 'A' && *c <= 'Z'));
+    return ((*c | 0x20) >= 'a' && (*c | 0x20) <= 'z');
 }
 
-// CHAR* 
-//--------// EasyCpy, EasyNCpy, EasyCat, EasyUpper, EasyLower, EasyRev | TODO : EasyNCat, EasyInsert, EasySplit
-// EasySwapcase, EasyCapitalize , EasySearchChr, EasyRSearchChr, EasySearchStr, EasyRSearchStr
 
-char * EasyCpy(char* destination,const char* source) {
+// CHAR* 
+//--------// EasyCpy, EasyNCpy, EasyCat, EasyUpper, EasyLower, EasyRev, EasySplit, EasySwapcase, EasyCapitalize, EasyInsert  | TODO : EasyNCat,
+//EasySearchChr, EasyRSearchChr, EasySearchStr, EasyRSearchStr
+
+
+char* EasyCpy(char* destination, const char* source) {
     char* orig = destination;
     while (*source) {
-        *destination++= *source++;
+        *destination++ = *source++;
     }
     *destination = '\0';
     return orig;
 }
 
-char * EasyNCpy(char* destination, const char* source,int n) {
+char* EasyNCpy(char* destination, const char* source, int n) {
     if (n <= 0) return destination;
     char* orig = destination;
     while (*source && n > 0) {
@@ -94,7 +98,7 @@ char * EasyNCpy(char* destination, const char* source,int n) {
     return orig;
 }
 
-char * EasyCat(char* destination, char* source) {
+char* EasyCat(char* destination, char* source) {
     char* orig = destination;
     while (*destination) {
         destination++;
@@ -106,52 +110,63 @@ char * EasyCat(char* destination, char* source) {
     return orig;
 }
 
-char* EasyUpper(char* destination) {
+char* EasyNCat(char* destination, char* source, int n) {
     char* orig = destination;
     while (*destination) {
-        if (*destination >= 'a' && *destination <= 'z') {
-            *destination -= 32;
+        destination++;
+    }
+    while (*source && n) {
+        *destination++ = *source++;
+        n--;
+    }
+    *destination = '\0';
+    return orig;
+}
+
+char* GeneralCase(char* destination, STRING_MODE MODE) {
+    char* orig = destination;
+    _Bool flag = 0;
+    while (*destination) {
+        switch (MODE)
+        {
+        case UPPER:
+            if (*destination >= 'a' && *destination <= 'z') {
+                *destination -= 32;
+            }
+            break;
+        case LOWER:
+            if (*destination >= 'A' && *destination <= 'Z') {
+                *destination += 32;
+            }
+            break;
+        case SWAP:
+            if (EasyIsLet(destination)){
+                *destination ^= 0x20;
+            }
+            break;
+        case CAP:
+            if (EasyIsLet(destination)) {
+                if (!flag) {
+                    *destination &= ~(0x20);
+                    flag = 1;
+                }
+                else {
+                    *destination |= 0x20;
+                }
+            }
+            break;
+        default:
+            break;
         }
         destination++;
     }
     return orig;
 }
 
-char* EasyLower(char* destination) {
-    char* orig = destination;
-    while (*destination) {
-        if (*destination >= 'A' && *destination <= 'Z') {
-            *destination += 32;
-        }
-        destination++;
-    }
-    return orig;
-}
-
-char* EasySwapcase(char* string) {
-    char* orig = string;
-    while (*string) {
-        if (*string >= 'a' && *string <= 'z') *string -= 32;
-        else if (*string >= 'A' && *string <= 'Z') *string += 32;
-        string++;
-    }
-}
-
-char* EasyCapitalize(char* string) {
-    char* orig = string;
-    _Bool first = 0;
-    while (*string) {
-        if (!first && EasyIsLet(string)){
-            *string -= 32;
-            first = 1;
-            string++;
-            continue;
-        }
-        if (*string >= 'A' && *string <= 'Z') *string += 32;
-        string++;
-    }
-    return orig;
-}
+char* EasyLower(char* dest) { return GeneralCase(dest, LOWER); }
+char* EasyUpper(char* dest) { return GeneralCase(dest, UPPER); }
+char* EasySwapcase(char* dest) { return GeneralCase(dest, SWAP); }
+char* EasyCapitalize(char* dest) { return GeneralCase(dest, CAP); }
 
 char* EasyRev(char* string) {
     int len = EasyLen(string);
@@ -162,10 +177,15 @@ char* EasyRev(char* string) {
     return  orig;
 }
 
-
-
-char* EasyInsert(char* destination, char* source, const int location) {
-    return;
+char* EasyInsert(char* destination, char* source, int location) {
+    char* temp = malloc(sizeof(char)*(EasyLen(destination)+EasyLen(source)+1));
+    EasyNCpy(temp, destination, location);
+    temp[location] = '\0';
+    EasyCat(temp, source);
+    EasyCat(temp, destination + location);
+    EasyCpy(destination, temp);
+    free(temp);
+    return destination;
 }
 
 char** memAlloc(char** newArr, char* origWord, const int len, const int wordCounter) {
@@ -184,15 +204,17 @@ char** memAlloc(char** newArr, char* origWord, const int len, const int wordCoun
     return newArr;
 }
 
-char ** EasySplit(char** newArr,const char* source,const char* sep , int* wordCounter)
+char** EasySplit(char** newArr, const char* source, const char* sep, int* wordCounter)
 {
+    
+    const char* origWord = source;
     const char* origSource = source;
     const char* origSep = sep;
-    const char* origWord = source;
-    const int len = EasyLen(sep);
+
     *wordCounter = 0;
 
-    while(*source)
+
+    while (*source)
     {
         while (*source == *sep && *sep)
         {
@@ -200,14 +222,14 @@ char ** EasySplit(char** newArr,const char* source,const char* sep , int* wordCo
             sep++;
         }
 
-        if ((source - origSource == len))
+        if (!*sep)
         {
             while (source > origSource)
             {
                 origSource++;
             }
             (*wordCounter)++;
-            newArr = memAlloc(newArr, origWord, source - origWord - len, (*wordCounter));
+            newArr = memAlloc(newArr, origWord, source - origWord - (sep - origSep), (*wordCounter));
             origWord = origSource;
         }
         else
@@ -228,24 +250,14 @@ int main() {
     char text[] = "test test test";
     char text2[] = "a test or something";
     char text4[] = "bACalAurEat";
-    char text3[20]= "ThIs is ";
+    char text3[20] = "ThIs is ";
+    char text5[30] = "iluminati";
 
+    char** arr = NULL;
+    int words;
 
-
-
-
-
-
-    char** newArr = NULL;
-    int counter;
-
-    newArr = EasySplit(newArr,"ana are multe, multe mere si pere"," ",&counter);
-
-
-    //Afisare cuvinte
-    for (int i = 0; i < counter; i++) {
-        printf("%s\n", newArr[i]);
-    }
+    EasyRev(text5);
+    printf("%s", text5);
 }
 
 
